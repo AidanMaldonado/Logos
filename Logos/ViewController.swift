702914266f6f2f2e0f765df0a7d534f0a6038062
Aidan Maldonado <aidanm1198@gmail.com>
2019-01-30 13:27:21 -0500

    Updated Game Files

--- Logos/ViewController.swift
+++ Logos/ViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     //Game Variables
+    //Default Values
     var days = 10
     var water = 10 //In hours
     var textInfo = "You are lost alone in the great sands"
     var gameTurn = 240 //Each hour is a tick
+    //Not changeable on restart
     var gameTile = ["Shack", "Nothing but Sand", "Oasis"]
     var uiToggle = true
+    var restartGame = false
     
     //Game Labels & Buttons
     @IBOutlet weak var daysLeftLabel: UILabel!
@@ -36,38 +39,65 @@ class ViewController: UIViewController {
     
     //Determines the current UI toggle and hides or unhides the UI
     func hideUIToggle() {
-        switch uiToggle {
-        case uiToggle:
+        if uiToggle {
             daysLeftLabel.isHidden = true
             waterLabel.isHidden = true
-            uiToggle = !uiToggle
-        default:
+            uiToggle = false
+        } else {
             daysLeftLabel.isHidden = false
             waterLabel.isHidden = false
-            uiToggle = !uiToggle
+            uiToggle = true
         }
     }
     
     //Player death screen
-    func playerDied(deathType: String) {
+    func playerDied(of deathType: String) {
         textInfo = "You died of \(deathType)"
         hideUIToggle()
         travelButtonLabel.setTitle("Restart", for: .normal)
+        restartGame = true
     }
     
-    @IBAction func travelButton() {
-        water -= 1
-        if water <= 0 {
-            playerDied(deathType: "Dehydration")
-        }
-        //Checks if one day has passed
-        gameTurn -= 1
-        if gameTurn % 24 == 0 {
-            days -= 1
-        }
+    //Resets variables
+    func restartGameFunc() {
+        hideUIToggle()
+        //Default Values
+        days = 10
+        water = 10
+        textInfo = "You are lost alone in the great sands"
+        gameTurn = 240
+        travelButtonLabel.setTitle("Travel", for: .normal)
         updateLabels()
+        restartGame = false
+    }
+    
+    func nextLocation() {
+        let rand = Int.random(in: 0...gameTile.count)
+    }
+    
+    //Action on travel
+    //Decrements water and gameturn values
+    @IBAction func travelButton() {
+        //If game needs to be restarted, restart game otherwise run as normal
+        if restartGame == true {
+            restartGameFunc()
+        } else {
+            water -= 1
+            //Checks if water has run out
+            if water <= 0 {
+                playerDied(of: "Dehydration")
+            }
+            //Checks if one day has passed
+            gameTurn -= 1
+            if gameTurn % 24 == 0 {
+                days -= 1
+            }
+            
+            updateLabels()
+        }
     }
 
 
 }
+
 
