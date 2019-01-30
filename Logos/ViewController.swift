20b671a65df271930f7e73eb857163d1776e334d
Aidan Maldonado <aidanm1198@gmail.com>
2019-01-30 09:58:08 -0500

    Initial Commit

---
+++ Logos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Logos
+//
+//  Created by Aidan Maldonado on 1/30/19.
+//  Copyright © 2019 Aidan Maldonado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
