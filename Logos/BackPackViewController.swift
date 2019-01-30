c8825bcea76997b6b37d4212879c821afabe7615
Aidan Maldonado <aidanm1198@gmail.com>
2019-01-30 14:39:47 -0500

    Backpack view controller

---
+++ Logos/BackPackViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BackPackViewController.swift
+//  Logos
+//
+//  Created by Aidan Maldonado on 1/30/19.
+//  Copyright © 2019 Aidan Maldonado. All rights reserved.
+//
+
+import UIKit
+
+class BackPackViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
